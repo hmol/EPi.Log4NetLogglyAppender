232e019a7add41fc1a5a25c811d3ab2dacc0d968
Dzulqarnain Nasir <dzul1983@gmail.com>
2014-08-19 14:07:29 +0300

    Initial commit

---
+++ Appender.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Net.Http;
+using log4net.Appender;
+using log4net.Core;
+using Newtonsoft.Json;
+
+namespace Log4NetLogglyAppender
+{
+    public class Appender : AppenderSkeleton
+    {
+        private readonly Process _currentProcess = Process.GetCurrentProcess();
+        private string _endpoint;
+
+        /// <summary>
+        /// Loggly endpoint for submitting log events.
+        /// </summary>
+        public string Endpoint
+        {
+            get { return _endpoint; }
+            set
+            {
+                _endpoint = value;
+                if (!_endpoint.EndsWith("/"))
+                    _endpoint += "/";
+                if (!_endpoint.EndsWith("inputs/"))
+                    _endpoint += "inputs/";
+            }
+        }
+
+        /// <summary>
+        /// Loggly customer token.
+        /// https://www.loggly.com/docs/customer-token-authentication-token/
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Comma-delimited list of tags to add to each log event.
+        /// Relevant only for Loggly API generation 2+
+        /// https://www.loggly.com/docs/tags/
+        /// </summary>
+        public string Tags { get; set; }
+
+        public Appender() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tags">Collection of additional tags to add to each log event.</param>
+        public Appender(IEnumerable<string> tags)
+        {
+            if (!string.IsNullOrWhiteSpace(Tags))
+                Tags += ",";
+
+            Tags += string.Join(",", tags);
+        }
+
+        /// <summary>
+        /// Appender itself
+        /// </summary>
+        /// <param name="loggingEvent">The internal representation of logging events.</param>
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            dynamic payload = new ExpandoObject();
+            payload.level = loggingEvent.Level.DisplayName;
+            payload.time = loggingEvent.TimeStamp.ToString("o");
+            payload.machine = Environment.MachineName;
+            payload.process = _currentProcess.ProcessName;
+            payload.thread = loggingEvent.ThreadName;
+            payload.message = loggingEvent.RenderedMessage;
+            payload.logger = loggingEvent.LoggerName;
+
+            var exception = loggingEvent.ExceptionObject;
+            if (exception != null)
+            {
+                payload.exception = new ExpandoObject();
+                payload.exception.message = exception.Message;
+                payload.exception.type = exception.GetType().Name;
+                payload.exception.stackTrace = exception.StackTrace;
+                if (exception.InnerException != null)
+                {
+                    payload.exception.innerException = new ExpandoObject();
+                    payload.exception.innerException.message = exception.InnerException.Message;
+                    payload.exception.innerException.type = exception.InnerException.GetType().Name;
+                    payload.exception.innerException.stackTrace = exception.InnerException.StackTrace;
+                }
+            }
+
+            var client = new HttpClient();
+            var url = string.Format("{0}{1}{2}", Endpoint, Token, string.IsNullOrWhiteSpace(Tags) ? string.Empty : "/tag/" + Tags);
+            var payloadJson = JsonConvert.SerializeObject(payload);
+
+            client.PostAsync(url, new StringContent(payloadJson));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-log4net-loggly-appender
+Log4net Loggly Appender
 =======================
+
+A simple log4net appender for sending log entries to [Loggly.com](http://www.loggly.com).
+
+# Installation
+
+Install the package via NuGet.
+
+After installing the NuGet package, you will need to register the appender in your Web.config file. Please replace the token value with your own Loggly token.
+
+```xml
+<log4net>
+    <appender name="loggly" type="Log4NetLogglyAppender.Appender, Log4NetLogglyAppender">
+        <endpoint value="https://logs.loggly.com/"/>
+        <token value="REPLACE-THIS-WITH-YOUR-LOGGLY-TOKEN"/>
+    </appender>
+
+    <root>
+        <level value="Warn"/>
+
+        <appender-ref ref="loggly"/>
+    </root>
+</log4net>
+
+```
+
+That's it. It should transmit everything you log using log4net to your Loggly.com feed.
